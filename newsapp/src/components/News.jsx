7a1e54b0d8644a43da33dd8f1a34ca88bdbb7b23
Rishab garg <rishabg546@gmail.com>
2023-09-24 22:12:17 +0530

    Third commit

--- newsapp/src/components/News.jsx
+++ newsapp/src/components/News.jsx
@@ -3,20 +3,51 @@ import NewsItem from "./NewsItem";
 import axios from "axios";
 
 export class News extends Component {
+  static defaultProps = {
+    category: "general",
+  };
   constructor() {
     super();
     console.log("This is news constructor");
     this.state = {
       article: [],
       loading: false,
+      page: 1,
     };
   }
   async componentDidMount() {
     const details = await axios.get(
-      "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=5795dbd941e34264b005fe612a5dfb1e"
+      `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=5795dbd941e34264b005fe612a5dfb1e&pageSize=20`
     );
     this.setState({ article: details.data.articles });
   }
+
+  handleNextClick = async () => {
+    const details = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=in&category=${
+        this.props.category
+      }&apiKey=5795dbd941e34264b005fe612a5dfb1e&page=${
+        this.state.page + 1
+      }&pageSize=20`
+    );
+    this.setState({
+      page: this.state.page + 1,
+      article: details.data.articles,
+    });
+  };
+  handlePreviousClick = async () => {
+    const details = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=in&category=${
+        this.props.category
+      }&apiKey=5795dbd941e34264b005fe612a5dfb1e&page=${
+        this.state.page - 1
+      }&pageSize=20`
+    );
+    this.setState({
+      page: this.state.page - 1,
+      article: details.data.articles,
+    });
+  };
 
   render() {
     return (
@@ -44,6 +75,23 @@ export class News extends Component {
                 </div>
               );
             })}
+          </div>
+          <div className="container d-flex justify-content-between">
+            <button
+              disabled={this.state.page <= 1}
+              type="button"
+              className="btn btn-primary"
+              onClick={this.handlePreviousClick}
+            >
+              Previous
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={this.handleNextClick}
+            >
+              Next
+            </button>
           </div>
         </div>
       </>
